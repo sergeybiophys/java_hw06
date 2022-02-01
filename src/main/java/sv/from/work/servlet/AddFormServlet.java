9b0a60d9e3e9fffb5dd4986feed7d58ac6cea46c
sergeybiophys <roven2hati@gmail.com>
2022-02-01 18:05:24 +0200

    changes in html2

--- src/main/java/sv/from/work/servlet/AddFormServlet.java
+++ src/main/java/sv/from/work/servlet/AddFormServlet.java
@@ -8,27 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AddFormServlet extends HttpServlet {
-  /*  @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        resp.setContentType("text/html");
-        resp.setCharacterEncoding("utf-8");
-        resp.getWriter().println("<div style=\""+"display: flex; width: 15%; margin: 0 auto; margin-top: 30px; background-color: lightblue; \""+">" +
-                "<form method='post' action='/add'>" +
-                "<label>UserName: <input style=\""+"margin-top: 30px; \"" +"name='username'/></label><br/>" +
-                "<label>E-mail: <input style=\""+"margin-left:25px; \"" + "name='email' type='email'/></label><br/>" +
-                "<label>Age: <input style=\""+"margin-left:42px; \"" + "name='age' type='number'/></label><br/>" +
-                "<input value='ADD-USER' style=\""+"margin-left:65px; margin-top: 30px; height: 30px; width: 100px \"" +"type='submit'/>" +
-                "</form>" +
-                "</div>");
-
-        Enumeration<String> parameterNames = req.getParameterNames();
-        if(parameterNames.hasMoreElements())
-        {
-            Connection.CreateUser(req.getParameter("username"), req.getParameter("email"),Integer.parseInt(req.getParameter("age")));
-        }
-
-    }*/
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
       request.getRequestDispatcher("/WEB-INF/pages/add.jsp").forward(request, response);
@@ -39,7 +19,6 @@ public class AddFormServlet extends HttpServlet {
         String email = request.getParameter("email");
         Integer age = Integer.parseInt(request.getParameter("age"));
         Integer groupId = Integer.parseInt(request.getParameter("group"));
-        //Student student = new Student(firstname, lastname, groupname);
         DbConnection.createUser(username,email,age,groupId);
         response.sendRedirect("/home");
     }
--- src/main/java/sv/from/work/servlet/HomeServlet.java
+++ src/main/java/sv/from/work/servlet/HomeServlet.java
@@ -11,67 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class HomeServlet extends HttpServlet {
-    /*@Override
-    public void init(ServletConfig config) throws ServletException {
-        //super.init(config);
-        System.out.println("Init HomeServlet");
-    }
 
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        System.out.println("Handle request");
-        resp.setContentType("text/html");
-        resp.setCharacterEncoding("UTF-8");
-        StringBuilder stringBuilder = new StringBuilder();
-        String idHeader = "ID ";
-        String userNameHeader = "User Name";
-        String emailHeader = "E-mail";
-        String ageHeader = "Age";
-
-       stringBuilder.append("<div style=\""+"display: flex; flex: wrap;  width: 60%; margin: 0 auto; background-color: grey; height: 35px; justify-content: space-around;\""+">");
-            stringBuilder.append("<div style=\""+"color:black; margin-right: 10px; \""+">"+idHeader+"</div>");
-
-
-            stringBuilder.append("<div>"+userNameHeader+"</div>");
-            stringBuilder.append("<div>"+emailHeader+"</div>");
-            stringBuilder.append("<div>"+ageHeader+"</div>");
-
-        stringBuilder.append("</div>");
-        //--------------------------------------------------------------------------------------------------------------
-
-
-        try{
-            ResultSet resultSet = Connection.GetAll();
-            while (resultSet.next()) {
-                System.out.println("iamhere");
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("username");
-                String email = resultSet.getString("email");
-                int age = resultSet.getInt("age");
-                System.out.printf("%3d | %-20s | %-20s | %-4d%n" , id, name, email,age);
-
-                stringBuilder.append("<div style=\""+"display: flex; flex: wrap; width: 60%; margin: 0 auto; margin-top: 20px;  background-color: white; height: 35px; justify-content: space-around; border: 1px solid lightblue;\""+">");
-                stringBuilder.append("<div style=\""+"color:red; margin-right: 10px; \""+">"+id+"</div>");
-                stringBuilder.append("<div>"+name+"</div>");
-                stringBuilder.append("<div>"+email+"</div>");
-                stringBuilder.append("<div>"+age+"</div>");
-
-                stringBuilder.append("</div>");
-            }
-        }
-        catch(SQLException e)
-        {
-            e.printStackTrace();
-        }
-        PrintWriter writer = resp.getWriter();
-        writer.println(stringBuilder.toString());
-    }
-
-    @Override
-    public void destroy() {
-        System.out.println("Destroy HomeServlet");
-    }*/
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
@@ -106,11 +46,6 @@ public class HomeServlet extends HttpServlet {
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //String firstname = req.getParameter("username");
-        //String lastname = req.getParameter("email");
-        //String groupname = request.getParameter("group");
-        //Student student = new Student(firstname, lastname, groupname);
-        //DataBase.CreateStudent(student);
-        //resp.sendRedirect("/");
+
     }
 }
